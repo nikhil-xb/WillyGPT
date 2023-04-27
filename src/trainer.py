dad1790e9bf5f45addff18b1fdfff4ecc6405e13
Nikhil Kumar <77227336+nikhil-xb@users.noreply.github.com>
2023-04-27 14:33:59 +0530

    error solved

--- src/trainer.py
+++ src/trainer.py
@@ -2,6 +2,7 @@ from .config import Config
 from .models import Final
 import torch
 from tqdm import tqdm
+import wandb
 
 class Trainer:
     def __init__(self, model, optim, loss_fn, wandb= False):
@@ -15,7 +16,7 @@ class Trainer:
             logits= self.model(input_)
             loss= self.loss_fn(logits, target)
             loss_item= loss.item()
-            if wandb:
+            if self.wandb:
                 wandb.log({'Train Loss': loss_item})
             self.optim.zero_grad(set_to_none=True)
             loss.backward()
@@ -26,7 +27,7 @@ class Trainer:
             logits= self.model(input_)
             loss= self.loss_fn(logits, target)
             loss_item= loss.item()
-            if wandb:
+            if self.wandb:
                 wandb.log({'Val_loss': loss_item})
         print(f"VLoss= {loss_item:.4f}")
         return loss_item, self.model
